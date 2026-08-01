c1f3bf2dd064e594bae1497ab31eda637bf9eb7b
Farhad-is-here <safarzade.farhad26@gmail.com>
2026-08-01 17:26:03 +0330

    edited internal controller

--- src/internal/internal.controller.ts
+++ src/internal/internal.controller.ts
@@ -1,12 +1,30 @@
 import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { InternalService } from './internal.service';
+import { CustomerService } from 'src/customer/customer.service';
+import { VendorService } from 'src/vendors/vendors.service';
 
 @Controller('internal')
 export class InternalController {
-  constructor(private internalService: InternalService) {}
+  constructor(
+    private customerService: CustomerService,
+    private VendorsService: VendorService,
+
+  ) {}
 
 @Get('users/:userId/profile')
-getProfile(@Param('userId', ParseIntPipe) userId: number) {
-  return this.internalService.getUserProfile(userId);
-}
+async getUserProfile(@Param('userId') userId: string) {
+    const customerProfile = await this.customerService
+      .findByUserId(parseInt(userId))
+      .catch(() => null);
+
+    const vendorProfile = await this.VendorsService
+      .findByUserId(parseInt(userId))
+      .catch(() => null);
+
+    return {
+      customerId: customerProfile?.id ?? null,
+      vendorId: vendorProfile?.id ?? null,
+      cityId: customerProfile?.cityId ?? vendorProfile?.cityId ?? null,
+    };
+  }
 }
